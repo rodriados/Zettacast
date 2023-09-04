20ed8fdebc9b15e2655d1ad728564a72698065b9
Rodrigo Siqueira <rodriados@gmail.com>
2023-09-03 23:30:25 -0400

    [refactor] Redefining project purpose and structure

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Zettacast
 ![license MIT](https://img.shields.io/badge/license-MIT-lightgrey.svg)
-![version pre-alpha](https://img.shields.io/badge/version-pre--alpha-green.svg)
-[![Build Status](https://travis-ci.org/rodriados/zettacast.svg?branch=master)](https://travis-ci.org/rodriados/zettacast)
+![version dev](https://img.shields.io/badge/version-dev-red.svg)
 [![Coverage Status](https://coveralls.io/repos/github/rodriados/zettacast/badge.svg)](https://coveralls.io/github/rodriados/zettacast)
 
-Zettacast is a simple, fast and lightweight PHP Framework. Zettacast is being made for those who want to have a powerful and straightforward tool for developing their websites without the - sometimes massive - overhead caused by other commonly used frameworks.
+Zettacast is a simple, fast and lightweight PHP dependency injection framework.
+
+## Motivation
+Zettacast is made for those who want to have a powerful and straightforward tool for dependency injection within their projects
+without the - sometimes massive - overhead caused by other commonly used frameworks, opening the possibility of
+integrating DI into already existing and complex projects.
+
+The API implemented by Zettacast tries to be very similar to that of [Guice](https://github.com/google/guice/tree/master),
+a dependency injection for Java developed by Google.
+
+## Install
+Once the project is released, the recommended way to install Zettacast will be via [Composer](http://getcomposer.org).
+
+```json
+{
+    "require": {
+        "rodriados/zettacast": "^1.0"
+    }
+}
+```
--- src/Zettacast.php
+++ src/Zettacast.php
@@ -1,104 +1,34 @@
 <?php
 /**
- * Zettacast\Zettacast class file.
- * @package Zettacast
+ * Zettacast: a simple, fast and lightweight PHP dependency injection.
  * @author Rodrigo Siqueira <rodriados@gmail.com>
+ * @copyright 2015-present Rodrigo Siqueira
+ * @package Zettacast
  * @license MIT License
- * @copyright 2015-2018 Rodrigo Siqueira
  */
 namespace Zettacast;
 
-use Zettacast\Injector\Binder;
-use Zettacast\Injector\Injector;
-use Zettacast\Support\SingletonTrait;
+use Exception;
 
 /**
- * The Zettacast class boots the framework and starts its main classes and
- * modules, allowing its correct usage and execution.
+ * The entry point to the Zettacast framework.
  * @version 1.0
  */
-final class Zettacast extends Injector
+final class Zettacast
 {
-	use SingletonTrait;
-	
-	/**
-	 * Informs Zettacast current version.
-	 * @var string Zettacast version.
-	 */
-	const VERSION = '1.0';
-	
-	/**#@+
-	 * Input mode constants. These constants inform whether the framework is
-	 * being executed by command line or by an user via a browser, or even if
-	 * it's an asynchronous request, such as AJAX.
-	 * @var int Input mode constants.
-	 */
-	const APP   = 0x010;
-	const CLI   = 0x020;
-	/**#@-*/
-	
-	/**#@+
-	 * Environment mode constants. These constants determine in which mode
-	 * Zettacast should execute. In some occasions, different actions are taken
-	 * depending on the environment framework is executing.
-	 * @var string Execution mode constants.
-	 */
-	const LOCAL         = 0x001;
-	const TESTING       = 0x002;
-	const DEVELOPMENT   = 0x004;
-	const PRODUCTION    = 0x008;
-	/**#@-*/
-	
-	/**
-	 * Zettacast constructor.
-	 * This method is responsible for setting the minimal configuration and
-	 * gathering information about the environment so the framework can work
-	 * correctly and execute the application as expected.
-	 */
-	public function __construct()
-	{
-		$binder = $this->helpers();
-		parent::__construct($binder);
-		
-		$this->set('path', APPPATH);
-		$this->set('path.base', ROOTPATH);
-		$this->set('path.public', WWWPATH);
-		$this->set('path.zetta', SRCPATH);
-		
-		$this->set(self::class, $this);
-		$this->set(Injector::class, $this);
-		
-		#set_error_handler([Handler::class, 'error']);
-		#set_exception_handler([Handler::class, 'exception']);
-		#register_shutdown_function([Handler::class, 'shutdown']);
-	}
-	
-	/**
-	 * Calls essential functions for the correct working of the framework as
-	 * intended. By doing this, kernels will only have to deal with
-	 * bootstrapping specific methods for their work.
-	 */
-	public function bootstrap()
-	{
-		#setlocale(LC_ALL, Config::get('app.locale', 'en_US'));
-		#mb_internal_encoding(Config::get('app.charset', 'UTF-8'));
-		#date_default_timezone_set(Config::get('app.timezone', 'UTC'));
-		
-		$this->set('mode', isset($_SERVER['argv']) ? self::CLI : self::APP);
-	}
-	
-	/**
-	 * Creates all shortcut aliases given by framework.
-	 * @return Binder The injector binder ready for usage.
-	 */
-	private function helpers(): Binder
-	{
-		$binder = new Binder;
-		$helpers = require SRCPATH.'/bindings.php';
-		
-		foreach($helpers as $binding)
-			$binder->bind(...$binding);
-		
-		return $binder;
-	}
+    /**
+     * Disallows the instantiation of the framework entry point.
+     * @see Zettacast::createInjector
+     */
+    private function __construct() {}
+
+    /**
+     * Creates a new injector from the given set of modules.
+     * @return never The function is not yet implemented.
+     * @throws Exception The function is not yet implemented.
+     */
+    public static function createInjector(): never
+    {
+        throw new Exception("function not yet implemented!");
+    }
 }
